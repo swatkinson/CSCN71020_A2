d3074cf2f9370db15974a05e8f01d9084918828d
swatkinson <spencerwatkinson@rogers.com>
2025-03-09 16:50:45 -0400

    Added perimeter and area unit tests

--- BCSRec.Tests/BCSRec.Tests.cpp
+++ BCSRec.Tests/BCSRec.Tests.cpp
@@ -11,17 +11,33 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace BCSRecTests
 {
-	TEST_CLASS(BCSRecTests)
+	TEST_CLASS(RectPerimeterTests)
 	{
 	public:
 		
-		TEST_METHOD(getPerimeterTest)
+		TEST_METHOD(getPerimeter_Length5Width10_Returns30)
 		{
 			int length = 5;
 			int width = 10;
 			
 			int result = getPerimeter(&length, &width);
 			int expected = 30;
+
+			Assert::AreEqual(expected, result);
+		}
+	};
+
+	TEST_CLASS(RectAreaTests)
+	{
+	public:
+
+		TEST_METHOD(getArea_Length5Width10_Returns50)
+		{
+			int length = 5;
+			int width = 10;
+
+			int result = getArea(&length, &width);
+			int expected = 50;
 
 			Assert::AreEqual(expected, result);
 		}
