8c3675abce88aac48c48730d69369ec77a9617e4
swatkinson <spencerwatkinson@rogers.com>
2025-03-09 17:29:11 -0400

    Added SetLength + SetWidth unit tests, 3 each

--- BCSRec.Tests/BCSRec.Tests.cpp
+++ BCSRec.Tests/BCSRec.Tests.cpp
@@ -15,6 +15,7 @@ namespace BCSRecTests
 	{
 	public:
 		
+		/// Tests that perimeter is correctly calculated
 		TEST_METHOD(getPerimeter_Length5Width10_Returns30)
 		{
 			int length = 5;
@@ -31,6 +32,7 @@ namespace BCSRecTests
 	{
 	public:
 
+		/// Tests that area is correctly calculated
 		TEST_METHOD(getArea_Length5Width10_Returns50)
 		{
 			int length = 5;
@@ -40,6 +42,82 @@ namespace BCSRecTests
 			int expected = 50;
 
 			Assert::AreEqual(expected, result);
+		}
+	};
+
+	TEST_CLASS(RectSetLengthTests)
+	{
+	public:
+
+		// Tests that length can be changed to a valid 1-99 number
+		TEST_METHOD(setLength_CurLen5NewLen50_ReturnsLength50)
+		{
+			int curLength = 5;
+			int newLength = 50;
+
+			setLength(newLength, &curLength);
+
+			Assert::AreEqual(curLength, newLength);
+		}
+
+		// Tests that length wont be changed to an invalid above-boundary range (100)
+		TEST_METHOD(setLength_CurLen50NewLen100_ReturnsLength50)
+		{
+			int curLength = 50;
+			int newLength = 100;
+
+			setLength(newLength, &curLength);
+
+			Assert::AreNotEqual(curLength, newLength);
+		}
+
+		// Tests that length wont be changed to an invalid below-boundary range (-1)
+		TEST_METHOD(setLength_CurLen75NewLenNegative1_ReturnsLength75)
+		{
+			int curLength = 75;
+			int newLength = -1;
+
+			setLength(newLength, &curLength);
+
+			Assert::AreNotEqual(curLength, newLength);
+		}
+	};
+
+	TEST_CLASS(RectSetWidthTests)
+	{
+	public:
+
+		// Tests that width can be changed to a valid 1-99 number
+		TEST_METHOD(setWidth_CurWidth5NewWidth50_ReturnsWidth50)
+		{
+			int curWidth = 5;
+			int newWidth = 50;
+
+			setLength(newWidth, &curWidth);
+
+			Assert::AreEqual(curWidth, newWidth);
+		}
+
+		// Tests that width wont be changed to an invalid above-boundary range (100)
+		TEST_METHOD(setWidth_CurWidth50NewWidth100_ReturnsWidth50)
+		{
+			int curWidth = 50;
+			int newWidth = 100;
+
+			setLength(newWidth, &curWidth);
+
+			Assert::AreNotEqual(curWidth, newWidth);
+		}
+
+		// Tests that width wont be changed to an invalid below-boundary range (-1)
+		TEST_METHOD(setWidth_CurWidth75NewWidthNegative1_ReturnsWidth75)
+		{
+			int curWidth = 75;
+			int newWidth = -1;
+
+			setLength(newWidth, &curWidth);
+
+			Assert::AreNotEqual(curWidth, newWidth);
 		}
 	};
 }
